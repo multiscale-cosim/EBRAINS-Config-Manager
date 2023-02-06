433c0590c0eb221b4725849a0e0a788a1374e0fe
ringleschavez <ringleschavez@gmail.com>
2023-02-06 17:56:27 +0100

    Transforming CO_SIM_SLURM_NODE_### into env values

--- workflow_configurations_manager/xml_parsers/services_deployment_xml_manager.py
+++ workflow_configurations_manager/xml_parsers/services_deployment_xml_manager.py
@@ -14,6 +14,7 @@
 
 # Co-Simulator's import
 from EBRAINS_ConfigManager.workflow_configurations_manager.xml_parsers import enums
+from EBRAINS_ConfigManager.workflow_configurations_manager.xml_parsers import utils
 from EBRAINS_ConfigManager.workflow_configurations_manager.xml_parsers import xml_tags
 # from EBRAINS_ConfigManager.workflow_configurations_manager.xml_parsers import constants
 from EBRAINS_ConfigManager.workflow_configurations_manager.xml_parsers.xml_manager import XmlManager
@@ -28,10 +29,11 @@ class ServicesDeploymentXmlManager(XmlManager):
              on local systems where multiple cores could be used.
     """
 
-    def __init__(self, log_settings, configurations_manager, xml_filename, name):
+    def __init__(self, log_settings, configurations_manager, variable_manager, xml_filename, name):
         super().__init__(log_settings, configurations_manager, xml_filename, name)
 
         self.__services_deployment_dict = None
+        self.__variable_manager = variable_manager
 
     def initialize_xml_elements(self):
         # TO BE DONE: there should be a global XML file where tags are defined
@@ -59,8 +61,8 @@ class ServicesDeploymentXmlManager(XmlManager):
 
         return enums.XmlManagerReturnCodes.XML_OK
 
-    def build_particular_sections_dicts(self):
-        return enums.XmlManagerReturnCodes.XML_OK
+    # def build_particular_sections_dicts(self):
+    #    return enums.XmlManagerReturnCodes.XML_OK
 
     def build_particular_sections_dicts(self):
         """
@@ -100,42 +102,17 @@ class ServicesDeploymentXmlManager(XmlManager):
             self._logger.error('{} has no <{}>...</{}> section'.format(self._xml_filename,
                                                                        xml_tags.CO_SIM_XML_CO_SIM_SERVICES_DEPLOYMENT_SETTINGS,
                                                                        xml_tags.CO_SIM_XML_CO_SIM_SERVICES_DEPLOYMENT_SETTINGS))
-        self.__services_deployment_dict[xml_tags.CO_SIM_XML_CO_SIM_SERVICES_DEPLOYMENT_SETTINGS] = \
-            self._main_xml_sections_dicts_dict[xml_tags.CO_SIM_XML_CO_SIM_SERVICES_DEPLOYMENT_SETTINGS]
+            return enums.XmlManagerReturnCodes.XML_TAG_ERROR
+        # self.__services_deployment_dict[xml_tags.CO_SIM_XML_CO_SIM_SERVICES_DEPLOYMENT_SETTINGS] = \
+        #    self._main_xml_sections_dicts_dict[xml_tags.CO_SIM_XML_CO_SIM_SERVICES_DEPLOYMENT_SETTINGS]
 
-        return enums.XmlManagerReturnCodes.XML_OK
+        # transforming the {CO_SIM_SLURM_NODE_###} into its actual values assigned by SLURM
+        for key, value \
+                in self.__services_deployment_dict[xml_tags.CO_SIM_XML_CO_SIM_SERVICES_DEPLOYMENT_SETTINGS].items():
 
-    def _TMP_build_particular_sections_dicts(self):
-        """
-            - Implements the abstract method which is called at the end of the
-            Co-Simulation XML file dissection process.
-            - Converts into INTEGER those members of the dictionary whose value is expected to be numerical.
-
-        :return:
-            XML_VALUE_ERROR -> When some string cannot be converted into INTEGER
-            XML_OK:         -> All the values were converted into INTEGER as expected.
-        """
-
-        #
-        # processing the communication dictionary with the following format:
-        # {
-        #   'ORCHESTRATOR': {'MIN': '59100', 'MAX': '59120', 'MAX_TRIES': '20'},
-        #   'COMMAND_CONTROL': {'MIN': '59121', 'MAX': '59150', 'MAX_TRIES': '30'},
-        #   'APPLICATION_COMPANION': {'MIN': '59150', 'MAX': '59200', 'MAX_TRIES': '50'}
-        # }
-        # 2022-09-21: So far all the components will be treated similarly,
-        #             meaning MAX MIN MAX_TRIES being INTEGER
-        #
-        for key, value in self._main_xml_sections_dicts_dict.items():
-            # key -> represents a CO-SIM Component, e.g. ORCHESTRATOR
-
-            try:
-                value['MIN'] = int(value['MIN'])
-                value['MAX'] = int(value['MAX'])
-                value['MAX_TRIES'] = int(value['MAX_TRIES'])
-            except ValueError:
-                self._logger.error(ValueError)
-                return enums.XmlManagerReturnCodes.XML_VALUE_ERROR
+            self.__services_deployment_dict[xml_tags.CO_SIM_XML_CO_SIM_SERVICES_DEPLOYMENT_SETTINGS][key] = \
+                utils.transform_co_simulation_variables_into_values(variables_manager=self.__variable_manager,
+                                                                    functional_variable_value=value)
 
         return enums.XmlManagerReturnCodes.XML_OK
 
